e7d151d7b4e394dc396a8c261866ccc49951455d
slaughtr <dslaughtr@gmail.com>
2017-04-07 14:19:23 -0700

    Add what will hopefully be the final .sql file

---
+++ wildlife_tracker.sql
@@ -0,0 +1,527 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.2
+-- Dumped by pg_dump version 9.6.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: animals; Type: TABLE; Schema: public; Owner: slaughtr
+--
+
+CREATE TABLE animals (
+    id integer NOT NULL,
+    name character varying,
+    health character varying,
+    age character varying,
+    image_url character varying
+);
+
+
+ALTER TABLE animals OWNER TO slaughtr;
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE; Schema: public; Owner: slaughtr
+--
+
+CREATE SEQUENCE animals_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE animals_id_seq OWNER TO slaughtr;
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: slaughtr
+--
+
+ALTER SEQUENCE animals_id_seq OWNED BY animals.id;
+
+
+--
+-- Name: endangered_animals; Type: TABLE; Schema: public; Owner: slaughtr
+--
+
+CREATE TABLE endangered_animals (
+    id integer NOT NULL,
+    name character varying,
+    health character varying,
+    age character varying,
+    image_url character varying
+);
+
+
+ALTER TABLE endangered_animals OWNER TO slaughtr;
+
+--
+-- Name: endangered_animals_id_seq; Type: SEQUENCE; Schema: public; Owner: slaughtr
+--
+
+CREATE SEQUENCE endangered_animals_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE endangered_animals_id_seq OWNER TO slaughtr;
+
+--
+-- Name: endangered_animals_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: slaughtr
+--
+
+ALTER SEQUENCE endangered_animals_id_seq OWNED BY endangered_animals.id;
+
+
+--
+-- Name: locations; Type: TABLE; Schema: public; Owner: slaughtr
+--
+
+CREATE TABLE locations (
+    id integer NOT NULL,
+    name character varying
+);
+
+
+ALTER TABLE locations OWNER TO slaughtr;
+
+--
+-- Name: locations_id_seq; Type: SEQUENCE; Schema: public; Owner: slaughtr
+--
+
+CREATE SEQUENCE locations_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE locations_id_seq OWNER TO slaughtr;
+
+--
+-- Name: locations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: slaughtr
+--
+
+ALTER SEQUENCE locations_id_seq OWNED BY locations.id;
+
+
+--
+-- Name: rangers; Type: TABLE; Schema: public; Owner: slaughtr
+--
+
+CREATE TABLE rangers (
+    id integer NOT NULL,
+    name character varying,
+    email character varying,
+    station_id integer
+);
+
+
+ALTER TABLE rangers OWNER TO slaughtr;
+
+--
+-- Name: rangers_id_seq; Type: SEQUENCE; Schema: public; Owner: slaughtr
+--
+
+CREATE SEQUENCE rangers_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE rangers_id_seq OWNER TO slaughtr;
+
+--
+-- Name: rangers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: slaughtr
+--
+
+ALTER SEQUENCE rangers_id_seq OWNED BY rangers.id;
+
+
+--
+-- Name: sightings; Type: TABLE; Schema: public; Owner: slaughtr
+--
+
+CREATE TABLE sightings (
+    id integer NOT NULL,
+    animal_id integer,
+    ranger_id integer,
+    last_sighting timestamp without time zone,
+    location_id integer
+);
+
+
+ALTER TABLE sightings OWNER TO slaughtr;
+
+--
+-- Name: sightings_animals; Type: TABLE; Schema: public; Owner: slaughtr
+--
+
+CREATE TABLE sightings_animals (
+    id integer NOT NULL,
+    animal_id integer,
+    sighting_id integer
+);
+
+
+ALTER TABLE sightings_animals OWNER TO slaughtr;
+
+--
+-- Name: sightings_animals_id_seq; Type: SEQUENCE; Schema: public; Owner: slaughtr
+--
+
+CREATE SEQUENCE sightings_animals_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE sightings_animals_id_seq OWNER TO slaughtr;
+
+--
+-- Name: sightings_animals_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: slaughtr
+--
+
+ALTER SEQUENCE sightings_animals_id_seq OWNED BY sightings_animals.id;
+
+
+--
+-- Name: sightings_id_seq; Type: SEQUENCE; Schema: public; Owner: slaughtr
+--
+
+CREATE SEQUENCE sightings_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE sightings_id_seq OWNER TO slaughtr;
+
+--
+-- Name: sightings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: slaughtr
+--
+
+ALTER SEQUENCE sightings_id_seq OWNED BY sightings.id;
+
+
+--
+-- Name: stations; Type: TABLE; Schema: public; Owner: slaughtr
+--
+
+CREATE TABLE stations (
+    id integer NOT NULL,
+    name character varying,
+    coordinates character varying,
+    location_id integer
+);
+
+
+ALTER TABLE stations OWNER TO slaughtr;
+
+--
+-- Name: stations_id_seq; Type: SEQUENCE; Schema: public; Owner: slaughtr
+--
+
+CREATE SEQUENCE stations_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE stations_id_seq OWNER TO slaughtr;
+
+--
+-- Name: stations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: slaughtr
+--
+
+ALTER SEQUENCE stations_id_seq OWNED BY stations.id;
+
+
+--
+-- Name: animals id; Type: DEFAULT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY animals ALTER COLUMN id SET DEFAULT nextval('animals_id_seq'::regclass);
+
+
+--
+-- Name: endangered_animals id; Type: DEFAULT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY endangered_animals ALTER COLUMN id SET DEFAULT nextval('endangered_animals_id_seq'::regclass);
+
+
+--
+-- Name: locations id; Type: DEFAULT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY locations ALTER COLUMN id SET DEFAULT nextval('locations_id_seq'::regclass);
+
+
+--
+-- Name: rangers id; Type: DEFAULT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY rangers ALTER COLUMN id SET DEFAULT nextval('rangers_id_seq'::regclass);
+
+
+--
+-- Name: sightings id; Type: DEFAULT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY sightings ALTER COLUMN id SET DEFAULT nextval('sightings_id_seq'::regclass);
+
+
+--
+-- Name: sightings_animals id; Type: DEFAULT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY sightings_animals ALTER COLUMN id SET DEFAULT nextval('sightings_animals_id_seq'::regclass);
+
+
+--
+-- Name: stations id; Type: DEFAULT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY stations ALTER COLUMN id SET DEFAULT nextval('stations_id_seq'::regclass);
+
+
+--
+-- Data for Name: animals; Type: TABLE DATA; Schema: public; Owner: slaughtr
+--
+
+COPY animals (id, name, health, age, image_url) FROM stdin;
+3	Rabbit	\N	\N	\N
+2	Deer	\N	\N	dag
+4	Deer	\N	\N	\N
+6	Raccoon	\N	\N	\N
+7	Fox	\N	\N	https://i.imgur.com/rwXStQ0.jpg
+8	Fox	\N	Adult	\N
+9	Deer	\N	\N	\N
+10	Black Bear	\N	\N	\N
+11	Fox	ill	\N	\N
+12	Fox	\N	\N	\N
+13	Deer	\N	\N	\N
+14	Black Bear	\N	\N	\N
+15	Deer	\N	\N	\N
+16	Deer	\N	\N	\N
+17	Deer	\N	\N	\N
+18	Deer	\N	\N	\N
+19	Badger	\N	\N	\N
+20	Deer	\N	\N	\N
+21	Deer	\N	\N	\N
+22	Deer	\N	\N	\N
+23	Badger	\N	\N	\N
+\.
+
+
+--
+-- Name: animals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: slaughtr
+--
+
+SELECT pg_catalog.setval('animals_id_seq', 23, true);
+
+
+--
+-- Data for Name: endangered_animals; Type: TABLE DATA; Schema: public; Owner: slaughtr
+--
+
+COPY endangered_animals (id, name, health, age, image_url) FROM stdin;
+3	Brown bear	Healthy	Young	\N
+4	Raccoon	Healthy	Young	https://i.imgur.com/rwXStQ0.jpg
+5	Fox	Healthy	Young	https://i.imgur.com/rwXStQ0.jpg
+6	Badger	Okay	Adult	https://i.imgur.com/rwXStQ0.jpg
+7	Fox	Healthy	Young	https://i.imgur.com/rwXStQ0.jpg
+8	Fox	Healthy	Adult	https://i.imgur.com/rwXStQ0.jpg
+9	Fox	Healthy	Young	https://i.imgur.com/rwXStQ0.jpg
+10	Fox	Healthy	Young	https://i.imgur.com/rwXStQ0.jpg
+11	Badger	Okay	Adult	https://i.imgur.com/rwXStQ0.jpg
+12	Fox	ill	Young	https://i.imgur.com/rwXStQ0.jpg
+13	Fox	Healthy	Young	https://i.imgur.com/rwXStQ0.jpg
+\.
+
+
+--
+-- Name: endangered_animals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: slaughtr
+--
+
+SELECT pg_catalog.setval('endangered_animals_id_seq', 13, true);
+
+
+--
+-- Data for Name: locations; Type: TABLE DATA; Schema: public; Owner: slaughtr
+--
+
+COPY locations (id, name) FROM stdin;
+\.
+
+
+--
+-- Name: locations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: slaughtr
+--
+
+SELECT pg_catalog.setval('locations_id_seq', 1, false);
+
+
+--
+-- Data for Name: rangers; Type: TABLE DATA; Schema: public; Owner: slaughtr
+--
+
+COPY rangers (id, name, email, station_id) FROM stdin;
+\.
+
+
+--
+-- Name: rangers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: slaughtr
+--
+
+SELECT pg_catalog.setval('rangers_id_seq', 1, false);
+
+
+--
+-- Data for Name: sightings; Type: TABLE DATA; Schema: public; Owner: slaughtr
+--
+
+COPY sightings (id, animal_id, ranger_id, last_sighting, location_id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: sightings_animals; Type: TABLE DATA; Schema: public; Owner: slaughtr
+--
+
+COPY sightings_animals (id, animal_id, sighting_id) FROM stdin;
+\.
+
+
+--
+-- Name: sightings_animals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: slaughtr
+--
+
+SELECT pg_catalog.setval('sightings_animals_id_seq', 1, false);
+
+
+--
+-- Name: sightings_id_seq; Type: SEQUENCE SET; Schema: public; Owner: slaughtr
+--
+
+SELECT pg_catalog.setval('sightings_id_seq', 1, false);
+
+
+--
+-- Data for Name: stations; Type: TABLE DATA; Schema: public; Owner: slaughtr
+--
+
+COPY stations (id, name, coordinates, location_id) FROM stdin;
+\.
+
+
+--
+-- Name: stations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: slaughtr
+--
+
+SELECT pg_catalog.setval('stations_id_seq', 1, false);
+
+
+--
+-- Name: animals animals_pkey; Type: CONSTRAINT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY animals
+    ADD CONSTRAINT animals_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: endangered_animals endangered_animals_pkey; Type: CONSTRAINT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY endangered_animals
+    ADD CONSTRAINT endangered_animals_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: locations locations_pkey; Type: CONSTRAINT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY locations
+    ADD CONSTRAINT locations_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: rangers rangers_pkey; Type: CONSTRAINT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY rangers
+    ADD CONSTRAINT rangers_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: sightings_animals sightings_animals_pkey; Type: CONSTRAINT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY sightings_animals
+    ADD CONSTRAINT sightings_animals_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: sightings sightings_pkey; Type: CONSTRAINT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY sightings
+    ADD CONSTRAINT sightings_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: stations stations_pkey; Type: CONSTRAINT; Schema: public; Owner: slaughtr
+--
+
+ALTER TABLE ONLY stations
+    ADD CONSTRAINT stations_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
